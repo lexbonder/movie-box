9201cedfcb6e0b7b0150d20155516e70bac73c11
Ricardo Viera <ricardoclean@gmail.com>
2018-02-07 09:24:12 -0700

    Store is now communicating
    Exported mapstostore, maptodispatch, connect to communicate correctly. Store is now getting movies

--- src/apiCall.js
+++ src/apiCall.js
@@ -1,13 +1,10 @@
 import apiKey from './apiKey.js';
-import updateMovie from './actions'
 
 const fetchApi = async() => {    
   try {
     const initalFetch = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`)
     const movieData = await initalFetch.json();
-    movieData.results.map(movie => {
-      updateMovie(movie)
-    })
+    return cleanData(movieData.results);
     
   } catch (error) {
     throw new Error('error');
@@ -15,7 +12,14 @@ const fetchApi = async() => {
 }
 
 const cleanData = (movieArray) => {
-
-}
+  return movieArray.map(movie => ({
+    title: movie.title,
+    date: movie.release_date,
+    overview: movie.overview,
+    poster: movie.poster_path,
+    backdrop: movie.backdrop_path,
+    id: movie.id
+  }));
+};
 
 export default fetchApi;
