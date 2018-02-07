0131d2f0b86ce34a6a6a97cdb911299dc1866d28
Nyssa Keller <nyssa1@gmail.com>
2018-02-06 18:20:01 -0700

    Add clean data function.
    Take api data and return clean object

--- src/App.js
+++ src/App.js
@@ -12,15 +12,27 @@ class App extends Component {
     try {
       const initalFetch = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`)
       const movieData = await initalFetch.json();
-      console.log(movieData.results);
+      this.cleanData(movieData.results)
     } catch (error) {
       throw new Error('error') 
     }
   }
 
+  cleanData = (movieArray) => {
+    movieArray.map(movie => ({
+        title: movie.title,
+        date: movie.release_date,
+        overview: movie.overview,
+        poster: movie.poster_path,
+        backdrop: movie.backdrop_path,
+        id: movie.id
+    }))
+  }
+
   render() {
     return (
       <div className="App">
+        <h1>movie tracker beyotch</h1>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,12 +4,22 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 
-const router = (
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-)
+// const store = createStore(rootReducer);
 
-ReactDOM.render(router, document.getElementById('root'));
+// const router = (
+//         <BrowserRouter>
+//             <App />
+//         </BrowserRouter>
+// )
+
+ReactDOM.render(
+    <App />,
+    // <Provider store={store}>router</Provider>, 
+    document.getElementById('root')
+);
+
 registerServiceWorker();
