6591465648289261b1a379f668f7f21153ba778c
Nyssa Keller <nyssa1@gmail.com>
2018-02-06 17:26:55 -0700

    Add fetch call in app on componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import apiKey from './apiKey.js';
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state ={}
+  }
+
+  componentDidMount = async() => {
+    try {
+      const initalFetch = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`)
+      const movieData = await initalFetch.json();
+      console.log(movieData.results);
+    } catch (error) {
+      console.log('error');
+      
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
