955bc4bda68544960907955077e7955b9165ab37
Nyssa Keller <nyssa1@gmail.com>
2018-02-07 17:28:23 -0700

    Test actions
    Test addMovie, toggleFavorite, and addFavorite

---
+++ src/actions/actions.test.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import * as actions from './index';
+
+
+describe('addMoive', () => {
+
+    it('return a type of ADD_MOVIE and with a movie', () => {
+        const movie = {
+            title: 'the greatest movie',
+            date: 'nunya',
+            overview: 'dogs are cute',
+            poster: 'fakepic.png',
+            id: 4
+        }
+        const expected = {
+            type: 'ADD_MOVIE',
+            movie
+        }
+        expect(expected).toEqual(actions.addMovie(movie));
+    });
+});
+
+describe('toggleFavorite', () => {
+
+    it('return a type of ADD_MOVIE and with a movie', () => {
+       const id = 0;
+        const expected = {
+            type: 'TOGGLE_FAVORITE',
+            id
+        }
+        expect(actions.toggleFavorite(id)).toEqual(expected);
+    });
+});
+
+describe('addFavorite', () => {
+
+    it('return a type of ADD_FAVORITE and with a movie', () => {
+        const movie = {
+            title: 'the greatest movie',
+            date: 'nunya',
+            overview: 'dogs are cute',
+            poster: 'fakepic.png',
+            id: 4
+        }
+        const expected = {
+            type: 'ADD_FAVORITE',
+            movie
+        }
+        expect(actions.addFavorite(movie)).toEqual(expected);
+    });
+});
